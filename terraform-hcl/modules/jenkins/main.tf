b1ef256cf334e91d7642099d06746d7e8d085648
Varid Vaya Yusuf <varidvaya@outlook.com>
2023-12-24 21:02:51 +0700

    Detatch network interface from EC2 instance #2 (#10)

--- terraform-hcl/modules/jenkins/main.tf
+++ terraform-hcl/modules/jenkins/main.tf
@@ -10,14 +10,6 @@ locals {
   }
 }
 
-resource "aws_network_interface" "network_interface_master" {
-  subnet_id       = var.subnet.id
-  security_groups = [var.security_group.id]
-  tags = merge(local.tags, {
-    Name = "${var.product}-${var.environment}-master"
-  })
-}
-
 resource "aws_instance" "instance_master" {
   associate_public_ip_address = true
   ebs_optimized               = false
@@ -26,15 +18,12 @@ resource "aws_instance" "instance_master" {
   instance_type               = "t2.micro"
   key_name                    = "serverless-jenkins-dev-master"
   monitoring                  = false
-  network_interface {
-    delete_on_termination = true
-    network_interface_id  = aws_network_interface.network_interface_master.id
-    device_index          = 0
-  }
   tags = merge(local.tags, {
     Name = "${var.product}-${var.environment}-master"
   })
   volume_tags = merge(local.tags, {
     Name = "${var.product}-${var.environment}-master"
   })
+  subnet_id = var.subnet.id
+  security_groups = [ var.security_group.id ]
 }
